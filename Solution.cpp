a35285e9e12664c3ac6dab56209648a0f9d6d4dd
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-04-03 08:22:15 +0200

    Create Solution.cpp

---
+++ Solution.cpp
@@ -0,0 +1,32 @@
+
+using namespace std;
+
+class Solution {
+    
+public:
+
+    bool validPalindrome(string s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s[left] != s[right]) {
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+private:
+
+    bool isPalindrome(const string& s, int left, int right) {
+        while (left < right) {
+            if (s[left++] != s[right--]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
